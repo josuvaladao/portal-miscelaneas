fffea1c4e55a03a342443e8affa2c5fa12591620
josuvaladao <104096659+josuvaladao@users.noreply.github.com>
2026-09-11 17:02:13 -0300

    Conecta portal ao novo projeto Apps Script de Josu

--- docs/config.js
+++ docs/config.js
@@ -1,4 +1,4 @@
 // Cole aqui a URL da implantação do aplicativo Google Apps Script, terminada em /exec.
 window.PORTAL_CONFIG = {
-  appUrl: "https://script.google.com/macros/s/AKfycbxst1z10rG8A1D27HzSJgGhV2k7A6ASSY2MHXaB3qkMT9LxrngLA6s-hRlISTjCHJXe3g/exec"
+  appUrl: "https://script.google.com/macros/s/AKfycbzHZap8lES7SP-_Cptst639enrV70TXzc8hzAJ6I4DnGmerCCDRr1OT-EVsAvjMvtRnRA/exec"
 };
